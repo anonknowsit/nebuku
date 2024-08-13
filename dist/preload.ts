7849ae4459e528bfb60b073a9be7dc88f2c23dab
Anonknowsit <anonknowsit@outlook.de>
2024-08-14 00:04:48 +0200

    init commit

--- dist/preload.ts
+++
@@ -1,7 +0,0 @@
-import { contextBridge, ipcRenderer } from "electron";
-
-contextBridge.exposeInMainWorld("electronAPI", {
-  minimize: () => ipcRenderer.send("minimize"),
-  maximize: () => ipcRenderer.send("maximize"),
-  close: () => ipcRenderer.send("close"),
-});
